3751ee08f683fc467996a2c9423ed37eb294a088
rithik singh <kushwahrithik@gmail.com>
2024-05-12 13:33:24 +0530

    updating new data

---
+++ src/Data/Products/Products.js
@@ -0,0 +1,26 @@
+export const ProductData = [
+    {
+        name: " New Balance 237 Sneakers For Men  (Green)",
+        price: "₹5,000",
+        discription: "Shipping charges are calculated based on the number of units, distance and delivery date.Delivery charges if applicable will be shown on the product page and cart.",
+        imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/t/p/q/-original-imagxy7wksb3yczu.jpeg?q=70&crop=false"
+    },
+    {
+        name: "237 Sneakers For Men  (White)",
+        price: "₹4,300",
+        discription: "Shipping charges are calculated based on the number of units, distance and delivery date.Delivery charges if applicable will be shown on the product page and cart.",
+        imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/d/u/q/-original-imagxwwqwwgtbgpq.jpeg?q=70&crop=false"
+    },
+    {
+        name: "237 Sneakers For Men  (Green)",
+        price: "₹5,000" ,
+        discription: "Shipping charges are calculated based on the number of units, distance and delivery date.Delivery charges if applicable will be shown on the product page and cart.",
+        imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/7/z/b/-original-imagxq8pgt6yad9g.jpeg?q=70&crop=false"
+    },
+    {
+        name: "574 Sneakers For Men  (Navy)",
+        price: "₹4,989",
+        discription: "Shipping charges are calculated based on the number of units, distance and delivery date.Delivery charges if applicable will be shown on the product page and cart.",
+        imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/shoe/o/b/p/-original-imagxq8pxhfncrj5.jpeg?q=70&crop=false"
+    }
+]
--- src/components/SectionThree/Section.jsx
+++ src/components/SectionThree/Section.jsx
@@ -1,32 +1,55 @@
 import React, { useContext } from 'react'
-import '../../App.css'
+import '../../App.css';
+import './Sectionthree.css';
 import { Productcontext } from '../../Context/ProductContext/Productcontext'
+import { ProductData } from '../../Data/Products/Products';
 const Section = () => {
-    const {initialCartNumber, UpdatedCartNumber,productData, UpdateProductData}=useContext(Productcontext);
-    console.log(productData)
-function handleClick(){
-    UpdatedCartNumber(initialCartNumber+1);
-    UpdateProductData(name)
+    //product data
     
-}
-let name = "Nike Air max";
+
+     
+    //importing context variables
+    const { initialCartNumber, UpdatedCartNumber, productData, UpdateProductData } = useContext(Productcontext);
+    console.log(productData)
+    function handleClick(event) {
+        console.log(event)
+        UpdatedCartNumber(initialCartNumber + 1);
+        // UpdateProductData(name)
+
+    }
+    // let name = "Nike Air max";
 
     return (
         <div className='section'>
             <h1>Products in Section</h1>
-
-            <div>
-                <div class="card" style={{"width": "18rem"}}>
-                    <img src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/e4060b19-289e-43b4-8375-c047c1cf6ab6/air-max-pulse-shoes-2bZSZV.png" class="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 class="card-title">{name}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary" onClick={handleClick}>Buy Now</a>
+            <div className='grid-container'>
+            {
+                ProductData.map((each,id) => {
+                    let nameForEachProduct = each.name;
+                    return (
+                       
+                        <div className="card grid-item" key={id} style={{ "width": "18rem" }}>
+                            <form onSubmit={handleClick} >
+                            <img src={each.imageUrl} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{nameForEachProduct}</h5>
+                                <p className="card-text">{each.discription}</p>
+                                <p className="card-text">{each.price}</p>
+                                <button  className="btn btn-primary" type='submit' >Buy Now</button>
+                            </div>
+                            </form>
                         </div>
-                </div>
-            </div>
-        </div>
+                      
+                    )
+                }
+
+
+                )
+            }
+              </div>
+        </div >
     )
+
 }
 
 export default Section
